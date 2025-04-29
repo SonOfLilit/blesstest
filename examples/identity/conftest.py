a44bc114449f2ba015cba3a91538110169bd6f84
Aur Saraf <aur@sarafconsulting.com>
2025-04-29 13:37:54 +0300

    test variations on harness

--- examples/identity/conftest.py
+++ examples/identity/conftest.py
@@ -7,6 +7,35 @@ class HarnessInput(pydantic.BaseModel):
     b: int
 
 
+class HarnessOutput(HarnessInput):
+    harness_name: str
+
+
 @harness
-def identity(test_input: HarnessInput) -> HarnessInput:
-    return test_input
+def identity(test_input: HarnessInput) -> HarnessOutput:
+    test_output = HarnessOutput(
+        a=test_input.a,
+        b=test_input.b,
+        harness_name="identity",
+    )
+    return test_output
+
+
+@harness
+def identity_2(test_input: HarnessInput) -> HarnessOutput:
+    test_output = HarnessOutput(
+        a=test_input.a,
+        b=test_input.b,
+        harness_name="identity_2",
+    )
+    return test_output
+
+
+@harness
+def identity_3(test_input: HarnessInput) -> HarnessOutput:
+    test_output = HarnessOutput(
+        a=test_input.a,
+        b=test_input.b,
+        harness_name="identity_3",
+    )
+    return test_output
