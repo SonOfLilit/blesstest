67f3aad2363272b1b93a4caf81a119dbe000151e
Yonatan Cale <myselfandfredy@gmail.com>
2025-05-01 13:59:29 +0300

    basic visidata viewer

--- examples/visidata-viewer/visidata_plugin.py
+++ examples/visidata-viewer/visidata_plugin.py
@@ -1,23 +1,35 @@
-from visidata import Sheet, ItemColumn, vd  # type: ignore # mypy can't find the types here
-
 # Run with:
 # uv run vd --config examples/visidata-viewer/visidata_plugin.py
 
+from visidata import Sheet, ItemColumn, vd  # type: ignore # mypy can't find the types here
+from pathlib import Path
+from diff import parse_patch
+from typing import Generator
+
 
 def do() -> None:
+    diff_file_path = Path(__file__).parent.joinpath("diff.patch")
+    diff_content = diff_file_path.read_text()
+
+    diffs = parse_patch(
+        diff_content,
+        context_lines=1,
+    )
+
     class SimpleSheet(Sheet):  # type: ignore # can't find the base Sheet type, mypy complcains, but doesn't run without inheriting from it
         columns = [
-            ItemColumn("col1", 0),
-            ItemColumn("col2", 1),
+            ItemColumn("file", 0),
+            ItemColumn("before", 1),
+            ItemColumn("after", 2),
         ]
 
-        def iterload(self) -> list[list[str]]:
-            return [
-                ["hello", "2"],
-                ["3", "4"],
-            ]
+        def iterload(self) -> Generator[tuple[str, str, str], None, None]:
+            for row in diffs.chunks:
+                yield row.after.file_path, row.before.content, row.after.content
 
     vd.push(SimpleSheet())
+    vd.run()
 
 
-do()
+if __name__ == "__main__":
+    do()
